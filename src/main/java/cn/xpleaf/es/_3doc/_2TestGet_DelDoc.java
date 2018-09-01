73ae0e31254bd804438ab273de5be873c2866175
leaf <yonghao@datastory.com.cn>
2018-09-01 17:08:48 +0800

    添加 查询删除 和 批量获取 测试到_2TestGet_DelDoc

--- src/main/java/cn/xpleaf/es/_3doc/_2TestGet_DelDoc.java
+++ src/main/java/cn/xpleaf/es/_3doc/_2TestGet_DelDoc.java
@@ -1,9 +1,16 @@
 package cn.xpleaf.es._3doc;
 
 import cn.xpleaf.es._1client.TestClient;
+import org.elasticsearch.action.bulk.byscroll.BulkByScrollResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetRequest;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.elasticsearch.index.reindex.DeleteByQueryRequestBuilder;
 import org.elasticsearch.rest.RestStatus;
 import org.junit.Before;
 import org.junit.Test;
@@ -57,6 +64,36 @@ public class _2TestGet_DelDoc {
         RestStatus status = res.status();
         System.out.println(status);
         // res.getXXX()有类似于创建文档时的其它api
+    }
+
+    // 查询删除
+    @Test
+    public void test03() throws Exception {
+        DeleteByQueryRequestBuilder builder = DeleteByQueryAction.INSTANCE.newRequestBuilder(client);
+        BulkByScrollResponse res = builder
+                .filter(QueryBuilders.matchQuery("name", "Joe Smith"))  // 删除的Query
+                .source("twitter")          // 索引名称
+                .get();
+        long deleted = res.getDeleted();    // 被删除文档的数目
+        System.out.println(deleted);
+    }
+
+    // 批量获取
+    @Test
+    public void test04() throws Exception {
+        MultiGetRequest.Item item = new MultiGetRequest.Item("another_index", "another_type", "anoterh_id");
+        MultiGetResponse multiGetItemResponses = client.prepareMultiGet()
+                .add("twitter", "tweet", "1")
+                .add("twitter", "tweet", "2", "3", "4")
+                .add(item)
+                .get();
+        for(MultiGetItemResponse itemResponse : multiGetItemResponses) {
+            GetResponse response = itemResponse.getResponse();
+            if(response !=null && response.isExists()) {
+                String content = response.getSourceAsString();
+                System.out.println(content);
+            }
+        }
     }
 
 }
